27dacd43c6c9863c7da882067148057ed9485530
Evgenii Bykovskii <bykovsky.ssau@gmail.com>
2025-01-18 17:55:24 +0400

    add handlers

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import asyncio
+import logging
 from environs import Env
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 
@@ -16,7 +17,38 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer('Привет')
+    await message.reply('Привет!')
+
+
+@dp.message(F.text == 'Приветики')
+async def echo(message: Message):
+    await message.reply('Привет!')
+
+
+@dp.message(F.photo)
+async def echo(message: Message):
+    photo_id = message.photo[-1].file_id
+    await message.answer_photo(photo=photo_id)
+
+
+@dp.message(F.document)
+async def echo(message: Message):
+    await message.answer_document(document=message.document.file_id)
+
+
+@dp.message(F.audio)
+async def echo(message: Message):
+    await message.answer_audio(audio=message.audio.file_id)
+
+
+@dp.message(F.animation)
+async def echo(message: Message):
+    await message.answer_animation(animation=message.animation.file_id)
+
+
+@dp.message(F.from_user.id == 534211907)
+async def echo(message: Message):
+    await message.answer(f'Ваш ID: {message.from_user.id}')
 
 
 async def main():
@@ -24,4 +56,5 @@ async def main():
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
