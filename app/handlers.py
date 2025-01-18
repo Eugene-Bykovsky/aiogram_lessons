f7ed840d2b30296c5da5ec1065daea54af184224
Evgenii Bykovskii <bykovsky.ssau@gmail.com>
2025-01-18 18:37:38 +0400

    add user_router

---
+++ app/handlers.py
@@ -0,0 +1,41 @@
+from aiogram import F, Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+
+user_router = Router()
+
+
+@user_router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.reply('Привет!')
+
+
+@user_router.message(F.text == 'Приветики')
+async def echo(message: Message):
+    await message.reply('Привет!')
+
+
+@user_router.message(F.photo)
+async def echo(message: Message):
+    photo_id = message.photo[-1].file_id
+    await message.answer_photo(photo=photo_id)
+
+
+@user_router.message(F.document)
+async def echo(message: Message):
+    await message.answer_document(document=message.document.file_id)
+
+
+@user_router.message(F.audio)
+async def echo(message: Message):
+    await message.answer_audio(audio=message.audio.file_id)
+
+
+@user_router.message(F.animation)
+async def echo(message: Message):
+    await message.answer_animation(animation=message.animation.file_id)
+
+
+@user_router.message(F.from_user.id == 534211907)
+async def echo(message: Message):
+    await message.answer(f'Ваш ID: {message.from_user.id}')
--- run.py
+++ run.py
@@ -2,56 +2,17 @@ import asyncio
 import logging
 from environs import Env
 
-from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message
-from aiogram.filters import CommandStart
-
-env = Env()
-env.read_env()
-
-TELEGRAM_TOKEN = env.str('TELEGRAM_TOKEN')
-
-bot = Bot(token=TELEGRAM_TOKEN)
-dp = Dispatcher()
-
-
-@dp.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.reply('Привет!')
-
-
-@dp.message(F.text == 'Приветики')
-async def echo(message: Message):
-    await message.reply('Привет!')
-
-
-@dp.message(F.photo)
-async def echo(message: Message):
-    photo_id = message.photo[-1].file_id
-    await message.answer_photo(photo=photo_id)
-
-
-@dp.message(F.document)
-async def echo(message: Message):
-    await message.answer_document(document=message.document.file_id)
-
-
-@dp.message(F.audio)
-async def echo(message: Message):
-    await message.answer_audio(audio=message.audio.file_id)
-
-
-@dp.message(F.animation)
-async def echo(message: Message):
-    await message.answer_animation(animation=message.animation.file_id)
-
-
-@dp.message(F.from_user.id == 534211907)
-async def echo(message: Message):
-    await message.answer(f'Ваш ID: {message.from_user.id}')
+from aiogram import Bot, Dispatcher
+from app.handlers import user_router
 
 
 async def main():
+    env = Env()
+    env.read_env()
+    TELEGRAM_TOKEN = env.str('TELEGRAM_TOKEN')
+    bot = Bot(token=TELEGRAM_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(user_router)
     await dp.start_polling(bot)
 
 
